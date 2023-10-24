171406394c82dd8b6599a5e9c1fc449a106804c0
dyeboah70 <dyeboah@mesika.org>
2023-10-24 16:18:42 +0100

    tag update

--- app/recipe/tests/test_tags_api.py
+++ app/recipe/tests/test_tags_api.py
@@ -21,6 +21,10 @@ def create_user(**params):
     return get_user_model().objects.create_user(**default_params)
 
 
+def detail_url(tag_id):
+    return reverse('recipe:tag-detail', args=[tag_id])
+
+
 class PublicTagsApiTests(TestCase):
     """Test the publicly available tags API"""
 
@@ -66,3 +70,15 @@ class PrivateTagsApiTests(TestCase):
         self.assertEqual(len(res.data), 1)
         self.assertEqual(res.data[0]['name'], tag.name)
         self.assertEqual(res.data[0]['id'], tag.id)
+
+    def test_update_tag(self):
+        """Test updating a tag"""
+        tag = Tag.objects.create(user=self.user, name='After Dinner')
+
+        payload = {'name': 'Dessert'}
+        url = detail_url(tag.id)
+        res = self.client.patch(url, payload)
+
+        self.assertEqual(res.status_code, status.HTTP_200_OK)
+        tag.refresh_from_db()
+        self.assertEqual(tag.name, payload['name'])
