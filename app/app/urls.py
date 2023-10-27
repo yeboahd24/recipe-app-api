fb898f74972e33b31c8b4de63cc7be3dc830af2d
dyeboah70 <dyeboah@mesika.org>
2023-10-27 10:18:53 +0100

    imagefield added to recipe

--- app/app/urls.py
+++ app/app/urls.py
@@ -15,6 +15,8 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
 from drf_spectacular.views import (
     SpectacularAPIView,
     SpectacularSwaggerView,
@@ -31,3 +33,8 @@ urlpatterns = [
     path('api/user/', include('user.urls', namespace='user')),
     path('api/recipe/', include('recipe.urls', namespace='recipe')),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(
+        settings.MEDIA_URL, document_root=settings.MEDIA_ROOT
+    )
